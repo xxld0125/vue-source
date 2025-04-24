74796e3402ffd1446737c8b7a0bd6275dfcb712d
xulf01 <xulf01@mingyuanyun.com>
2025-04-24 10:36:58 +0800

    feat: 添加代码测试覆盖率检查

--- vitest.config.js
+++ vitest.config.js
@@ -8,6 +8,12 @@ export default defineConfig({
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],
+      thresholds: {
+        statements: 90,
+        branches: 85,
+        functions: 90,
+        lines: 90,
+      },
     },
   },
 });
